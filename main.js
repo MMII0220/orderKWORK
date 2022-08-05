6965fb490e6db92aeac9a98cde35b8d22d014527
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-08-05 22:21:42 +0300

    upgraded nav__link

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const bar = document.querySelector('.icon__link'),
     link = document.querySelectorAll('.nav__link'),
     nav = document.querySelector('.nav'),
-    icon__bar = document.querySelector('.icon__link');
+    icon__bar = document.querySelector('.icon__link'), header = document.querySelector(".header");
 
   let i = 0;
 
@@ -15,6 +15,8 @@ window.addEventListener('DOMContentLoaded', () => {
         nav.style.height = '100vh';
         nav.style.animationName = 'fade';
         nav.style.animationDuration = '.3s';
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+        header.style.background = "rgba(0,0,0,0.4)";
         icon__bar.innerHTML =
           '<i class="fa-solid fa-x" style="font-size: calc(16px + 1vh);"></i>';
       });
@@ -25,6 +27,8 @@ window.addEventListener('DOMContentLoaded', () => {
         nav.style.height = '0';
         nav.style.animation = 'none';
         nav.style.background = 'none';
+        document.body.style.backgroundColor = "white";
+        header.style.background = "white";
         icon__bar.innerHTML = '<i class="fa-solid fa-bars">';
       });
     }
